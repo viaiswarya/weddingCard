c21a2cb3ebed97fd7ecea2db22a04e56b642e989
viaiswarya <87668138+viaiswarya@users.noreply.github.com>
2025-08-11 23:09:58 +0530

    countdown done

--- js/script.js
+++ js/script.js
@@ -46,7 +46,7 @@ function animateOpen(){
  
 
 // Set the date we're counting down to
-var countDownDate = new Date("Nov 29, 2022 08:45:00").getTime();
+var countDownDate = new Date("Aug 31, 2025 10:30:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
